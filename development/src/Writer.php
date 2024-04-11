b94bd1abab4847b07b9c1fe2e76312b1bcc93301
Sebastian Iancu <sebastian-iancu@users.noreply.github.com>
2024-04-11 11:08:00 +0200

    stamping version 3.1.0 in xml files; regenerating XML and FHIR files

--- development/src/Writer.php
+++ development/src/Writer.php
@@ -8,6 +8,7 @@ class Writer {
     public static function save(string $name, string $content): void {
         $basename = basename($name);
         echo "Writer: saving $basename - ";
+        $name = str_replace('fhir', 'FHIR', $name);
         $bytes = file_put_contents($name, $content);
         echo "($bytes bytes) done!" . PHP_EOL;
     }
